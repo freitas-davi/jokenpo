60ada6b0600b192ecd127a3a7e7cc12b5c21eb38
freitas-davi <freitas.davi@sempreceub.com>
2025-10-16 09:42:09 -0300

    feat: Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.model.Mao;
+import org.example.model.Resultado;
+import org.example.service.JokenpoService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        Mao primeiraMao = new Mao(JokenpoService.getRandomGesto());
+        Mao segundaMao = new Mao(JokenpoService.getRandomGesto());
+
+        Resultado resultado = JokenpoService.avaliarPartida(primeiraMao, segundaMao);
+
+        String retorno = JokenpoService.retorno(resultado);
+
+        System.out.println("Resultado: " + primeiraMao.gesto().nome() + retorno + segundaMao.gesto().nome());
     }
 }
